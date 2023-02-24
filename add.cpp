646b61c33a348787edd16473d2934d4fbc219be8
Kritika <126231890+kritika2201s@users.noreply.github.com>
2023-02-24 01:42:31 -0800

    Add files via upload
    first

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    int c;
+
+    cin>>a >>b >>c;
+    c=a+b;
+    cout<<c;
+
+    return 0;
+}
